29f62a95ade08c5f691569de444797f342d942cd
Maicon Gonzaga <sacmaiconchip@gmail.com>
2021-03-12 14:50:10 -0400

    Correções

--- cadastrar/CadTermometro.php
+++ cadastrar/CadTermometro.php
@@ -18,14 +18,14 @@ if(!empty($_POST['cod']) && !empty($_POST['t_minimo']) && !empty($_POST['t_maxim
 
         if(mysqli_query($con, $sql))
         {
-            echo "<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=https://termonline.com.br/admin/addTermometro.php'>
+            echo "<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=https://localhost/admin/addTermometro.php'>
             <script type='text/javascript'> alert('Termometro salva com sucesso!');
             </script>
             ";
         }
         else
         {
-            echo "<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=https://termonline.com.br/admin/addTermometro.php'>
+            echo "<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=https://localhost/admin/addTermometro.php'>
             <script type='text/javascript'>
             alert('Erro ao salvar Termometro');
             </script>
@@ -35,10 +35,10 @@ if(!empty($_POST['cod']) && !empty($_POST['t_minimo']) && !empty($_POST['t_maxim
     else if(!empty($_POST['u_minimo']) && !empty($_POST['u_maximo']))
     {
         $sql = "INSERT INTO termometro(cod, descricao, t_minimo, t_maximo, u_minimo, u_maximo, fk_cliente_cnpj, fk_status_termometro_cod) VALUES ('$cod','$descricao',$t_minimo,$t_maximo,$u_minimo,$u_maximo,'$cnpj',1)";
-        
+
         if(mysqli_query($con, $sql))
         {
-            echo "<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=https://termonline.com.br/admin/addTermometro.php'>
+            echo "<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=https://localhost/admin/addTermometro.php'>
             <script type='text/javascript'> alert('Termometro salva com sucesso!');
             </script>
             ";
@@ -46,7 +46,7 @@ if(!empty($_POST['cod']) && !empty($_POST['t_minimo']) && !empty($_POST['t_maxim
         else
         {
             echo "
-            <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=https://termonline.com.br/admin/addTermometro.php'>
+            <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=https://localhost/admin/addTermometro.php'>
             <script type='text/javascript'>
             alert('Erro ao salvar Termometro');
             </script>
@@ -55,7 +55,7 @@ if(!empty($_POST['cod']) && !empty($_POST['t_minimo']) && !empty($_POST['t_maxim
     }
     else
         echo "
-        <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=https://termonline.com.br/admin/addTermometro.php'>
+        <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=https://localhost/admin/addTermometro.php'>
         <script type='text/javascript'>
         alert('Verifique se todos os campos foram preenchidos');
         </script>
@@ -63,7 +63,7 @@ if(!empty($_POST['cod']) && !empty($_POST['t_minimo']) && !empty($_POST['t_maxim
 }
 else
     echo "
-        <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=https://termonline.com.br/admin/addTermometro.php'>
+        <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=https://localhost/admin/addTermometro.php'>
         <script type='text/javascript'>
         alert('Verifique se todos os campos foram preenchidos');
         </script>
---
+++ editar/editarHorario.php
@@ -0,0 +1,40 @@
+<?php
+require "../classes/verifica.php";
+require_once ('../classes/conexao.php');
+$cnpj = $_SESSION['usuarioCNPJ'];
+if((isset($_POST['hora'])) && (isset($_POST['termometro'])) && (isset($_POST['minuto'])) && (isset($_GET['id'])))
+{
+    $id         = $_GET['id'];
+    $termometro = $_POST['termometro'];
+    $hora       = $_POST['hora'];
+    $minuto     = $_POST['minuto'];
+    $horario    = $hora . ":" . $minuto . ":00";
+
+    $sql = "UPDATE horarios SET horario='$horario', fk_termometro_cod='$termometro'
+            WHERE id_horario=$id AND fk_cliente_cnpj='$cnpj'";
+
+    if(mysqli_query($con, $sql))
+    {
+        header("Location: ../listarHorarios.php");
+    }
+    else
+    {
+        echo "
+            <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=https://localhost/admin/listarHorarios.php?id=. $id .'>
+            <script type='text/javascript'>
+            alert('Erro ao atualizar horario');
+            </script>
+            ";
+    }
+
+}
+else
+{
+    echo "
+        <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=https://localhost/admin/listarHorarios.php'>
+        <script type='text/javascript'>
+        alert('Todos os campos obrigatorios precisam ser preencidos');
+        </script>
+        ";
+}
+?>
